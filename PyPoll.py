aac2e55f09842e0827d8d35552af08da09d1d735
unknown <you@example.com>
2020-08-09 09:41:32 -0400

    Adding the election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -8,16 +8,77 @@
 # csv module
 import csv
 import os
-dir(csv)
+dir(os)
 
+# initialize variables
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 # assigning variable to file path
 load1 = os.path.join("resources", "election_results.csv")
 save1 = os.path.join("analysis", "election_analysis.txt")
+outfile1 = open(save1, "w")
+election_results_header = (
+    f"Elections Results Summary\n"
+    f"----------------------------\n")
+
+print(election_results_header)
+outfile1.write(election_results_header)
+
 with open(load1,"r") as election_results:
     #Read, Analyze
     file_reader = csv.reader(election_results)
     # Print header row
     headers = next(file_reader)
-    print(headers)
+
+    for row in file_reader:
+        #Sum votes
+        total_votes += 1
+
+        #specify where the candidate name is found in the csv
+        candidate_name = row[2]
+
+        #if not in list of candidate
+        if candidate_name not in candidate_options:
+
+            #then add them
+            candidate_options.append(candidate_name)
+
+            #initialize vote tallying
+            candidate_votes[candidate_name] = 0
     
+        #tally votes over each row iteration
+        candidate_votes[candidate_name] += 1
+
+for candidate_name in candidate_votes:
+
+    #name votes value
+    votes = candidate_votes[candidate_name]
+
+    #calculate percentages as float
+    vote_percentage = float(votes) / float(total_votes) * 100
+
+    #print candidate names and voting percentages in sentences. Round to nearest hundreth
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})")
+    outfile1.write(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+    #update winner
+    if votes > winning_count:
+        winning_candidate = candidate_name
+        winning_percentage = vote_percentage
+        winning_count = votes
+
+winning_candidate_summary = (
+    f"----------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"----------------------------\n")
+
+print(winning_candidate_summary)
+outfile1.write(winning_candidate_summary)
+
 election_results.close
